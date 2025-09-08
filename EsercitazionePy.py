4ac3c60506eeac590fdc1db448608edcba7c688e
Luigi <lc71@libero.it>
2025-09-08 12:12:05 +0200

    Esercizio 3

--- EsercitazionePy.py
+++ EsercitazionePy.py
@@ -25,7 +25,7 @@ Chiedi all'utente di inserire due numeri.
 Utilizza gli operatori aritmetici per sommare, sottrarre, moltiplicare e dividere i numeri.
 Stampa i risultati di ciascuna operazione. """
 
-print("Inserisci dei numeri per ottenere: somma, sottrazione, moltiplicazione e divisione")
+""" print("Inserisci dei numeri per ottenere: somma, sottrazione, moltiplicazione e divisione")
 primo_numero = int(input("Inserisci il primo numero: "))
 secondo_numero = int(input("inserisci il secondo numero: "))
 
@@ -37,4 +37,15 @@ divisione = primo_numero / secondo_numero
 print("La somma di ", primo_numero, " + ",secondo_numero, "è:", somma)
 print("La sottrazione di", primo_numero, " - ", secondo_numero, "è:", sottrazione)
 print("La moltiplicazione di ", primo_numero, " x ", secondo_numero,  "è:", moltiplicazione)
-print("La divisione di ", primo_numero, " / ", secondo_numero, "è:", divisione)
+print("La divisione di ", primo_numero, " / ", secondo_numero, "è:", divisione) """
+
+
+""" -------------------------------------------------------- """
+""" Esercizio 3: Saluto perosnalizzato
+Chiedi all'utente di inserire il proprio nome.
+Utilizza print() per creare un saluto personalizzato.
+Esempio di output: "Ciao alice! Benvenuto/a!"  """
+
+print("Dimmi come ti chiami....")
+nome = input("Inserisci il tuo nome: ")
+print("Ciao", nome, "! Benvenuto/a nell'esercizio 3!")
