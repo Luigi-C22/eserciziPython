23bdeb9471b534e7266ed89bc324d46b29d03621
Luigi <lc71@libero.it>
2025-09-08 12:05:54 +0200

    Esercizi 1 e 2

---
+++ EsercitazionePy.py
@@ -0,0 +1,40 @@
+""" Le VARIABILI """
+""" Esercizio 1: Assegnazione di variabili
+Crea tre variabili per rappresentare il nome, l'età e la città di una persona.
+Stampa ciascuna variabile separatamente.
+Cambia il valore di una variabile e ristampala """
+nome = "Luigi"
+eta = 54
+citta = "Roma"
+
+print(nome)
+print(eta)
+print(citta)
+
+nome = "Giorgio"
+eta = 51
+citta = "Milano"
+
+print(nome)
+print(eta)
+print(citta)
+
+""" ---------------------------------------------- """
+""" Esercizio 2: Calcolatrice Base
+Chiedi all'utente di inserire due numeri.
+Utilizza gli operatori aritmetici per sommare, sottrarre, moltiplicare e dividere i numeri.
+Stampa i risultati di ciascuna operazione. """
+
+print("Inserisci dei numeri per ottenere: somma, sottrazione, moltiplicazione e divisione")
+primo_numero = int(input("Inserisci il primo numero: "))
+secondo_numero = int(input("inserisci il secondo numero: "))
+
+somma = primo_numero + secondo_numero
+sottrazione = primo_numero - secondo_numero
+moltiplicazione = primo_numero * secondo_numero
+divisione = primo_numero / secondo_numero
+
+print("La somma di ", primo_numero, " + ",secondo_numero, "è:", somma)
+print("La sottrazione di", primo_numero, " - ", secondo_numero, "è:", sottrazione)
+print("La moltiplicazione di ", primo_numero, " x ", secondo_numero,  "è:", moltiplicazione)
+print("La divisione di ", primo_numero, " / ", secondo_numero, "è:", divisione)
